c6246047bdf7cac82b67b772a9ac2e06678a425a
= <ledeme.florian@gmail.com>
2020-03-24 21:00:15 +0100

    maj pour checkstyle

--- src/main/java/florianldm/App.java
+++ src/main/java/florianldm/App.java
@@ -1,13 +1,16 @@
 package florianldm;
 
 /**
- * Hello world!
+ * Hello world!.
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+
+    /**
+     * Méthode main.
+     * @param args arguments.
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
     }
 }
